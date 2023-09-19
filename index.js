47940f3900952bb731153c75b891c12972d22811
Andree52 <andreearter@pursuit.org>
2023-09-18 21:34:15 -0400

    javalava

--- index.js
+++ index.js
@@ -4,7 +4,32 @@
  * Depending on the operation, either add up all of the numbers or subtract all of the numbers, from left to right.
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
-function calculator() {}
+function calculator(num1,num2) {
+    let opp = process.argv[2];
+    let result = 0;
+    let nums = process.argv.slice(3);
+    if (!opp) {
+        return `No operation provided...`;
+    }
+    if (!nums.length) {
+        return `No numbers provided...`
+    }
+    if (opp !== `plus` && opp !== `minus`) {
+        return `Invalid operation: ${opp}`
+    }
+    if (opp ===`plus`) {
+        for (const vers of nums ) {
+           result += Number(vers) 
+        }
+    }
+    if (opp === `minus`) {
+        result = nums[0]
+        for (let H = 1; H < nums.length; H++) {
+           result -= Number(nums[H]) 
+            
+        }
+    } return result
+}
 
 // Don't change anything below this line.
 module.exports = calculator;
